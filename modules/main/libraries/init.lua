d35f3072075a06dfa6580fd8f5d75b40d7484030
chickenmst <mgeek602@gmail.com>
2025-05-03 16:33:46 +1000

    typo in init for libraries

--- modules/main/libraries/init.lua
+++ modules/main/libraries/init.lua
@@ -1,5 +1,5 @@
 -- init library table
 modules.main.libraries = {} -- table of library functions
 
-require "modules.main.libraries.loging" -- load the loging library
+require "modules.main.libraries.logging" -- load the loging library
 require "modules.main.libraries.chat" -- load the chat library
