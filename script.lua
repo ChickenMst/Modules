eea98ac036ed3ae313016ad593ef93a49412de9e
chickenmst <mgeek602@gmail.com>
2025-05-04 11:48:38 +1000

    removed depirated functions form the main script.lua

--- script.lua
+++ script.lua
@@ -1,19 +1,12 @@
 require "modules"
---- Called when the script is initialized (whenever creating or loading a world.)
----@param is_world_create boolean Only returns true when the world is first created.
-function onCreate(is_world_create)
-	modules.libraries.logging:info("onCreate()", "Script loaded")
-end
 
---- Called every game tick
----@param game_ticks number the number of ticks since the last onTick call (normally 1, while sleeping 400.)
-function onTick(game_ticks)
-	modules:Tick() -- run the tick function for all loaded addons
-end
-
---- Called when the world is exited.
-function onDestroy()
-end
+modules.libraries.callbacks:connect("onCreate", function(is_world_create)
+	if is_world_create then
+		modules.libraries.logging:info("onCreate()", "World created")
+	else
+		modules.libraries.logging:info("onCreate()", "Script reloaded")
+	end
+end)
 
 modules.libraries.callbacks:connect("onChatMessage", function(peer_id, sender_name, message)
 	modules.libraries.logging:info("onChatMessage()", "Player: " .. sender_name .. " sent a message: " .. message)
