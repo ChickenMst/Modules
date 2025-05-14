346861bb8b6c301667ee0b9fce5427e30580104e
ChickenMst <mgeek602@gmail.com>
2025-05-14 10:32:58 +1000

    added debugung to addon handler

--- modules/services/addons.lua
+++ modules/services/addons.lua
@@ -7,15 +7,17 @@ modules.libraries.callbacks:connect("onCreate", function(is_world_create)
         ---@param name string
         for name, addon in pairs(modules.libraries.addons.addons) do
             if addon.enabled then
-                modules.libraries.logging:info("services.addons", "Loading addon: "..name) -- print to the console
-                local addonloaded = addon.func:init() -- enable the addon
+                modules.libraries.logging:debug("services.addons", "Loading addon: "..name)
+                local addonloaded = addon.func:init() -- runs the init function of the addon
                 if addonloaded then
-                    modules.libraries.logging:info("services.addons", "Addon "..name.." loaded") -- print to the console
+                    modules.libraries.logging:debug("services.addons", "Addon "..name.." loaded")
                 else
-                    modules.libraries.logging:error("services.addons", "Addon "..name.." failed to load") -- print to the console
+                    modules.libraries.logging:error("services.addons", "Addon "..name.." failed to load, disableing addon")
+                    modules.libraries.addons:disable(name) -- disable the addon if it fails to load
+                    modules.libraries.logging:debug("services.addons", "Addon "..name.." disabled")
                 end
             else
-                modules.libraries.logging:info("services.addons", "Skiped loading addon: "..name..", addon is not enabled") -- print to the console
+                modules.libraries.logging:debug("services.addons", "Skiped loading addon: "..name..", addon is not enabled")
             end
         end
     end
