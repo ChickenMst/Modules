a289d8ae200b47992ddaee9db074e1026a6212bb
ChickenMst <mgeek602@gmail.com>
2025-05-19 11:36:16 +1000

    add connection to require

--- modules/classes/init.lua
+++ modules/classes/init.lua
@@ -4,3 +4,4 @@ require "modules.classes.event" -- load the event class
 require "modules.classes.addon" -- load the addon class
 require "modules.classes.loop" -- load the loop class
 require "modules.classes.command" -- load the command class
+require "modules.classes.connection" -- load the connection class
