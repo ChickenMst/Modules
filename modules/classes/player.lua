1cbeaf14e0303be50fecb281f37ef49cd8b31d16
ChickenMst <mgeek602@gmail.com>
2025-06-11 09:07:21 +1000

    add extra to player class and intelisence

--- modules/classes/player.lua
+++ modules/classes/player.lua
@@ -1,14 +1,23 @@
 modules.classes.player = {} -- table of player functions
 
-function modules.classes.player:create(peerId, steamId, name, admin, auth, perms)
+---@param peerId string|number
+---@param steamId string|number
+---@param name string|nil
+---@param admin boolean|nil
+---@param auth boolean|nil
+---@param perms table|nil
+---@param extra table|nil
+---@return Player
+function modules.classes.player:create(peerId, steamId, name, admin, auth, perms, extra)
     ---@class Player
     local player = {
-        peerId = peerId,
-        steamId = steamId,
+        peerId = tostring(peerId),
+        steamId = tostring(steamId),
         name = name or "Unknown",
         admin = admin or false,
         auth = auth or false,
         perms = perms or {},
+        extra = extra or {}
     }
 
     function player:setName(newName)
@@ -21,6 +30,20 @@ function modules.classes.player:create(peerId, steamId, name, admin, auth, perms
 
     function player:setAuth(isAuth)
         self.auth = isAuth
+    end
+
+    function player:setExtra(key, value)
+        if not self.extra then
+            self.extra = {}
+        end
+        self.extra[key] = value
+    end
+
+    function player:getExtra(key)
+        if not self.extra then
+            return nil
+        end
+        return self.extra[key]
     end
 
     function player:setPerm(perm, value)
