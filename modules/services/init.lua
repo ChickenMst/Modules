2594f9b254a9b0cc17af0e632394141f1769d17b
ChickenMst <mgeek602@gmail.com>
2025-06-14 16:01:56 +1000

    rename loops service to loop

--- modules/services/init.lua
+++ modules/services/init.lua
@@ -2,7 +2,7 @@
 modules.services = {}
 
 require "modules.services.addons" -- load the addons service
-require "modules.services.loops" -- load the loops service
+require "modules.services.loop" -- load the loops service
 require "modules.services.commands" -- load the commands service
 require "modules.services.vehicles" -- load the vehicles service
 require "modules.services.player" -- load the player service
--- modules/services/loop.lua
+++ modules/services/loop.lua
@@ -1,19 +1,19 @@
-modules.services.loops = {} -- table of loop functions
+modules.services.loop = {} -- table of loop functions
 
-modules.services.loops.loops = {} -- table of loops
+modules.services.loop.loops = {} -- table of loops
 
 -- create a new loop
 ---@param time number
 ---@param func function
-function modules.services.loops:create(time, func)
-    local loop = modules.classes.loop:create(time, func, #modules.services.loops.loops)
+function modules.services.loop:create(time, func)
+    local loop = modules.classes.loop:create(time, func, #modules.services.loop.loops)
 
     self.loops[loop.id] = loop
 end
 
 -- delete a loop
 ---@param id number
-function modules.services.loops:remove(id)
+function modules.services.loop:remove(id)
     if self.loops[id] then
         self.loops[id] = nil
     end
@@ -22,7 +22,7 @@ end
 -- connect into onTick
 modules.libraries.callbacks:connect("onTick", function ()
     local timeNow = server.getTimeMillisec()
-	for _, v in pairs(modules.services.loops.loops) do
+	for _, v in pairs(modules.services.loop.loops) do
 		if timeNow >= v.creationTime + (v.time * 1000) and not v.paused then
 			v.callback()
 			v.creationTime = timeNow
